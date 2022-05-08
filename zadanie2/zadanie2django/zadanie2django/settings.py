42a6342319381497a79340a8e895120280800e94
Tomas Berezny <tberezny@inno.innovatrics.net>
2022-05-08 23:26:32 +0200

    zadanie 6

--- zadanie2/zadanie2django/zadanie2django/settings.py
+++ zadanie2/zadanie2django/zadanie2django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,6 +80,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'dota': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'), 
+        'USER': config('USER_NAME'), 
+        'PASSWORD': config('USER_PASSWORD'),
+        'HOST': config('DB_HOST'), 
+        'PORT': config('DB_PORT'),
     }
 }
 
